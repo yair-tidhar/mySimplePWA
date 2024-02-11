43ba7b70c4d7732dc626d1f042cf772b1c2a1df5
yair-tidhar <75252496+yair-tidhar@users.noreply.github.com>
2024-02-11 23:30:34 +0200

    Update gpic.js

--- js/gpic.js
+++ js/gpic.js
@@ -1,7 +1,23 @@
 
 var path = "imgs/meow.jpg";
 
+function whenAvailable(name, callback, max_rep) {
+    if (max_rep==0) return false;
+    var interval = 50; // ms
+    window.setTimeout(function() {
+        if (window[name]) {
+            callback(window[name]);
+	    return true;
+        } else {
+            return whenAvailable(name, callback, max_rep-1);
+        }
+    }, interval);
+}
+
 function init(){
+	if ( !whenAvailable('loaded', function(){}, 20) ){
+		return;
+	}
 	document.getElementById("initbut").remove();
 	loaded();
 	set_back(path);
