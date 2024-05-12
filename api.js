9c807530d06e771a193ea9d35f4105ba917f2280
yair.tidhar <yair.tidhar.42@gmail.com>
2024-05-12 17:25:04 +0300

    remove everything from git (except from imgs)

--- api.js
+++
@@ -1,8 +0,0 @@
-
-const FIELD_ID = "field";
-const PLAYER_HAND_ID = "player";
-const JOCKER_HAND_ID = "JockerHand"
-
-function getCardID(handID, indx){ //str, int -> HTML
-    return document.getElementByID(handID).children.item(indx);
-}
